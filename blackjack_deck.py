e6993ca5de31b6cf1c1c96340fd9bb3ae50a9e96
Cindy Shove <Cindy@Cindys-MacBook-Air.local>
2015-06-29 20:54:29 -0700

    working blackjack deck that looks prettier

--- blackjack_deck.py
+++ blackjack_deck.py
@@ -3,6 +3,13 @@
 # create a deck of cards for black jack that values aces as 11, and the face cards as 10.
 # all other cards--numbered 2 though 10--are valued at their face value
 
-blackjack_deck = {"hearts":{"Ace":11, "King":10, "Queen":10, "Jack":10, "Ten":10, "Nine":9, "Eight":8, "Seven":7, "Six":6, "Five":5, "Four": 4, "Three":3, "Two":2}, "spades": {"Ace":11, "King":10, "Queen": 10, "Jack": 10, "Ten": 10, "Nine": 9, "Eight":8, "Seven": 7, "Six": 6, "Five":5, "Four": 4, "Three":3, "Two":2},"diamonds": {"Ace":11, "King":10, "Queen": 10, "Jack": 10, "Ten": 10, "Nine": 9, "Eight":8, "Seven": 7, "Six": 6, "Five":5, "Four": 4, "Three":3, "Two":2},"clubs": {"Ace":11, "King":10, "Queen": 10, "Jack": 10, "Ten": 10, "Nine": 9, "Eight":8, "Seven": 7, "Six": 6, "Five":5, "Four": 4, "Three":3, "Two":2}}
+blackjack_deck = {"hearts":{"Ace":11, "King":10, "Queen":10, "Jack":10, "Ten":10, "Nine":9, "Eight":8, "Seven":7, 
+"Six":6, "Five":5, "Four": 4, "Three":3, "Two":2}, 
+"spades": {"Ace":11, "King":10, "Queen": 10, "Jack": 10, "Ten": 10, "Nine": 9, "Eight":8, "Seven": 7, 
+"Six": 6, "Five":5, "Four": 4, "Three":3, "Two":2},
+"diamonds": {"Ace":11, "King":10, "Queen": 10, "Jack": 10, "Ten": 10, "Nine": 9, "Eight":8, "Seven": 7, 
+"Six": 6, "Five":5, "Four": 4, "Three":3, "Two":2},"clubs": 
+{"Ace":11, "King":10, "Queen": 10, "Jack": 10, "Ten": 10, "Nine": 9, "Eight":8, "Seven": 7, 
+"Six": 6, "Five":5, "Four": 4, "Three":3, "Two":2}}
 
 print blackjack_deck
